d6939b34ff61b92b0c0338ee6197aed0ce7f5faf
Rivaldo Stuart Wattimena <aldojrwattimena@gmail.com>
2024-02-09 21:44:28 +0700

    add db and redis initialize

--- config/redis.go
+++ config/redis.go
@@ -10,3 +10,11 @@ type Redis struct {
 func (r *Redis) Addr() string {
 	return fmt.Sprintf("%s:%d", r.Host, r.Port)
 }
+
+func (r *Redis) Index() int {
+	return 0
+}
+
+func (r *Redis) ConfigInfo() string {
+	return fmt.Sprintf("%+v", r)
+}
